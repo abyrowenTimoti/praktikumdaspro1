b1c2020a26278a68dc0fa080ff825977d7d0e477
abyrowenTimoti <abyrowen@gmail.com>
2025-10-01 11:37:50 +0700

    Modifikasi percobaan 1

--- jobsheet5/ifCetakKRS01.java
+++ jobsheet5/ifCetakKRS01.java
@@ -13,7 +13,9 @@ public class ifCetakKRS01 {
     if (uktLunas) {
       System.out.println("Pembayaran UKT terverifikasi");
       System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-      
+    } else {
+      System.out.println("Registrasi ditolak");
+      System.out.println("Lunasi UKT terlebih dahulu");
     }
   }
 }
